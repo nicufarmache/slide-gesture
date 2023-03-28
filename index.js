a6573cf0fe2d3611a598a5284e435b784b56a330
Nicolae Farmache <gitlab@nicu.ro>
2023-03-28 20:05:00 +0200

    Initial commir

---
+++ index.js
@@ -0,0 +1,93 @@
+export class SlideGesture {
+  constructor(el, callback, { touchActions, stopScrollDirection } = {}){
+    this.el = el;
+    this.touchActions = touchActions;
+    this.startX = 0;
+    this.startY = 0;
+    this.lastTotalX = 0;
+    this.lastTotalY = 0;
+    this.callback = callback;
+    this.stopScrollDirection = stopScrollDirection;
+  }
+
+  addListeners() {
+    this.el.addEventListener("pointerdown", this.handleEvt.bind(this));
+    this.el.addEventListener("pointermove", this.handleEvt.bind(this));
+    this.el.addEventListener("pointerup", this.handleEvt.bind(this));
+    this.el.addEventListener("pointercancel", this.handleEvt.bind(this));
+    window.addEventListener("touchmove", this.handleScroll.bind(this), {passive:false});
+    if(this.touchActions) this.el.style.touchAction = this.touchActions;
+  }
+
+  removeListeners() {
+    this.el.removeEventListner("pointerdown", this.handleEvt.bind(this));
+    this.el.removeEventListner("pointermove", this.handleEvt.bind(this));
+    this.el.removeEventListner("pointerup", this.handleEvt.bind(this));
+    this.el.removeEventListner("pointercancel", this.handleEvt.bind(this));
+    window.removeEventListner("touchmove", this.handleScroll.bind(this));
+    if(this.touchActions) this.el.style.touchAction = null;
+  }
+
+  preventTouchScroll() {
+    this.shouldPreventScroll = true;
+  }
+
+  allowTouchScroll() {
+    this.shouldPreventScroll = false;
+  }
+
+  handleScroll(evt) {
+    if(this.shouldPreventScroll){
+      evt.preventDefault();
+    }
+  }
+
+  handleEvt(evt) {  
+    if (evt.type === 'pointerdown') {
+      this.el.setPointerCapture(evt.pointerId);
+      this.startX = evt.pageX;
+      this.startY = evt.pageY;
+    }
+
+    if (el.hasPointerCapture(evt.pointerId) && evt.type !== 'pointercancel' && (typeof this.callback) === 'function') {
+      const relativeX = evt.pageX - this.startX;
+      const relativeY = evt.pageY - this.startY;
+
+      if ( this.stopScrollDirection === 'horizontal' && Math.abs(relativeX / relativeY) > 1 ) {
+        this.preventTouchScroll();
+      }
+      if ( this.stopScrollDirection === 'vertical' && Math.abs(relativeX / relativeY) < 1 ) {
+        this.preventTouchScroll();
+      }
+
+      this.callback(evt, {
+        startX: this.startX,
+        startY: this.startY,
+        relativeX,
+        relativeY,
+        totalX: relativeX + this.lastTotalX,
+        totalY: relativeY + this.lastTotalY,
+      });
+    }
+  
+    if (evt.type === 'pointerup') {
+      this.lastTotalX = + this.lastTotalX + evt.pageX - this.startX;
+      this.lastTotalY = + this.lastTotalY + evt.pageY - this.startY;
+      this.el.releasePointerCapture(evt.pointerId);
+      this.allowTouchScroll();
+    }
+
+    if (evt.type === 'pointercancel') {
+      this.callback(evt, {
+        startX: this.startX,
+        startY: this.startY,
+        relativeX: 0,
+        relativeY: 0,
+        totalX: this.lastTotalX,
+        totalY: this.lastTotalY,
+      });
+      this.el.releasePointerCapture(evt.pointerId);
+      this.allowTouchScroll();
+    }
+  }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,96 @@
+
 # Slide Gesture
+
+This library that allows you to easly implement a sliding gesture on an element by using pointer events 
+
+
+
+## Usage/Examples
+### Install pakage with npm:
+
+```bash
+npm install @nicufarmache/slide-gesture
+```
+
+### Initialization
+
+```javascript
+import { SlideGesture } from '@nicufarmache/slide-gesture'
+
+const slideGesture = new SlideGesture(
+    el, // emelement to track
+    handler, // event handler 
+    { 
+      touchActions: 'pan-y', // allow vertical scrolling
+      stopScrollDirection: 'horizontal' // prevent scrolling after horizontal sliding starts
+    }
+);
+
+```
+### Start listening
+
+```javascript
+slideGesture.addListeners();
+```
+
+### Event Handler 
+
+```javascript
+function handler(
+  evt, // pointer event
+  extra // extra info
+) {
+  console.log(evt.type) // event type (pointerdown, pointermove, etc)
+
+  console.log(evt.pageX) // pointer position reltive to page
+  console.log(evt.pageY) // pointer position reltive to page
+
+  console.log(extra.startX) // pointer position at the begining of the slide
+  console.log(extra.startY) // pointer position at the begining of the slide
+
+  console.log(extra.relativeX) // pointer position difference since sliding began
+  console.log(extra.relativeY) // pointer position difference since sliding began
+
+  console.log(extra.totalX) // sum of position differences since initialisation
+  console.log(extra.totalY) // sum of position differences since initialisation
+}
+
+```
+
+### Example:
+```javascript
+import { SlideGesture } from '@nicufarmache/slide-gesture'
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const el = document.getElementById("el"); // element you want
+
+  const handler = (evt, extra) => {
+
+    if (evt.type === 'pointerdown') {
+      console.log('Started sliding!')
+    }
+
+    if (evt.type === 'pointermove') {
+        console.log('Sliding!')
+    }
+  
+    if (evt.type === 'pointerup') {
+      console.log('Stopped sliding!')
+    }
+
+    if (evt.type === 'pointercancel') {
+      console.log('Caneled sliding!')
+    }
+
+    el.style.transform =`translate(${extra.totalX}px, 0px)`; // move element 
+  };
+
+  const slideGesture = new SlideGesture(el, handler, { 
+    touchActions: 'pan-y', // allow vertical scrolling
+    stopScrollDirection: 'horizontal' // prevent scrolling after horizontal sliding starts
+  });
+  slideGesture.addListeners();
+
+});
+```
+
