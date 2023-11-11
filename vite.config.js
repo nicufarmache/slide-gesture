078caa4745ad1740f5327c8f7712d340c8c6fc20
Nicolae Farmache <gitlab@nicu.ro>
2023-11-11 01:42:09 +0100

    added ts and vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
+    target: 'esnext',
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'Slide Gesture',
-      fileName: 'slide-gesture',
+      fileName: 'index',
       formats: ["es", "cjs"],
     },
   },
+  plugins: [dts()],
 })
