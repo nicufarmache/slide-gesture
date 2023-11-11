e75e5697217f12b1e88832c9929f69fc573c0b33
Nicolae Farmache <gitlab@nicu.ro>
2023-11-11 01:42:32 +0100

    updated example

--- example/main.js
+++ example/main.js
@@ -1,27 +1,16 @@
 // import { SlideGesture } from 'https://unpkg.com/@nicufarmache/slide-gesture/index.js';
-import { SlideGesture } from '../dist/slide-gesture';
+import { SlideGesture } from '../dist/index.js';
 
-const el = document.getElementById("el");
-const handler = (evt, extra) => {
-
-  console.log('Main handleEvt', evt.type, extra.totalX, evt.pageX, extra);
-
-  el.style.transform =`translate(${extra.totalX}px, 0px)`;
+const manageVisualStuff = (el, evt, extra) => {
+  console.log('position:', evt.type, extra.totalX, evt.pageX, extra);
 
   if (evt.type === 'pointerdown') {
     el.classList.add('moving');
     evt.preventDefault();
   }
 
-  if (evt.type === 'pointermove') {
-
-  }
-
   if (evt.type === 'pointerup') {
-    setTimeout(() => {
-      el.classList.remove('moving');
-      el.classList.remove('canceled');
-    }, 50);
+    el.classList.remove('moving');
   }
 
   if (evt.type === 'pointercancel') {
@@ -33,7 +22,31 @@ const handler = (evt, extra) => {
   }
 };
 
-window.slideGesture = new SlideGesture(el, handler, { touchActions: 'pan-y', stopScrollDirection: 'horizontal' });
+const handlerFree = (evt, extra) => {
+  const el = evt.target.closest('.thing');
+  el.style.transform =`translate(${extra.totalX}px, ${extra.totalY}px)`;
+  manageVisualStuff(el, evt, extra);
+};
+
+const handlerHorizontal = (evt, extra) => {
+  const el = evt.target.closest('.thing');
+  el.style.transform =`translate(${extra.totalX}px, 0)`;
+  manageVisualStuff(el, evt, extra);
+};
+
+const handlerVertical = (evt, extra) => {
+  const el = evt.target.closest('.thing');
+  el.style.transform =`translate(0, ${extra.totalY}px)`;
+  manageVisualStuff(el, evt, extra);
+};
+
+const elFree = document.getElementById("free");
+const elHorizontal = document.getElementById("horizontal");
+const elVertical = document.getElementById("vertical");
+
+new SlideGesture(elFree, handlerFree);
+new SlideGesture(elHorizontal, handlerHorizontal, { touchActions: 'pan-y', stopScrollDirection: 'horizontal' });
+new SlideGesture(elVertical, handlerVertical, { touchActions: 'pan-x', stopScrollDirection: 'vertical' });
 
 
 
